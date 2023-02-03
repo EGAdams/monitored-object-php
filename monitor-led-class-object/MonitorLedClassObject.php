b8e90a42cdb48615f67f0117cf09fd7256121d00
egadams <31349790+EGAdams@users.noreply.github.com>
2023-02-02 20:48:04 -0500

    causing error.  commented out.

--- monitor-led-class-object/MonitorLedClassObject.php
+++ monitor-led-class-object/MonitorLedClassObject.php
@@ -1,12 +1,13 @@
 <?php
 /** @class MonitorLedClassObject */
 
-class MonitorLedClassObject {
-    public function __construct() {
-        $this->background_color = "lightyellow";
-        $this->text_align       = "left";
-        $this->margin_top       = "2px";
-        $this->color            = "black";
-    }
-} ?>
+// class MonitorLedClassObject {
+//     public function __construct() {
+//         $this->background_color = "lightyellow";
+//         $this->text_align       = "left";
+//         $this->margin_top       = "2px";
+//         $this->color            = "black";
+//     }
+// }
+?>
 
